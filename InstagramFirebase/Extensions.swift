c7ff32f20c427b615c75e029ab3b35ff64a0fea6
Roman Salazar Lopez <roman@Romans-MacBook-Pro.local>
2017-08-04 16:55:22 -0700

    added helper methods for the color and the anchoring

---
+++ InstagramFirebase/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by Roman Salazar Lopez on 8/4/17.
+//  Copyright © 2017 Roman Salazar Lopez. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}

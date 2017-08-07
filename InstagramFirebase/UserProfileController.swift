a8af7518e7c3edea68d6994fce61928934b2b02b
Roman Salazar Lopez <roman@Romans-MacBook-Pro.local>
2017-08-07 15:43:04 -0700

    fetching the username from Firebase DB and displaying it on as the title is working fine.

--- InstagramFirebase/UserProfileController.swift
+++ InstagramFirebase/UserProfileController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class UserProfileController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .green
+        collectionView?.backgroundColor = .white
+        navigationItem.title = Auth.auth().currentUser?.uid
+        fetchUser()
+    }
+    
+    fileprivate func fetchUser() {
+        
+        guard let uid = Auth.auth().currentUser?.uid else {return}
+        
+        Database.database().reference().child("users").child(uid).observe(.value, with: { (snapshot) in
+            guard let dictionary = snapshot.value as? [String : Any] else {return}
+            let username = dictionary["username"] as? String
+            self.navigationItem.title = username
+            print(snapshot.value ?? "")
+        }) { (error) in
+            print("failured to fetch user")
+        }
     }
 }

a1a56c62545032c43460b23bd18565b8733b265c
Roman Salazar Lopez <roman@Romans-MacBook-Pro.local>
2017-08-07 12:36:14 -0700

    Tab bar controller added

---
+++ InstagramFirebase/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  InstagramFirebase
+//
+//  Created by Roman Salazar Lopez on 8/7/17.
+//  Copyright © 2017 Roman Salazar Lopez. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+}

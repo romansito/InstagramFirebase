2512f740e469f868a5dd9d87f072f169b095367a
Roman Salazar Lopez <roman@Romans-MacBook-Pro.local>
2017-08-15 11:55:30 -0700

    added blue background for login page and working on adding the logo

--- InstagramFirebase/LoginController.swift
+++ InstagramFirebase/LoginController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    let logoContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.rgb(red: 0, green: 120, blue: 175, alpha: 1)
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        logoImageView.backgroundColor = .red
+        view.addSubview(logoImageView)
+        
+        return view
+    }()
+    
     let signupButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Don't have an account? Sign Up.", for: .normal)
@@ -22,10 +32,17 @@ class LoginController: UIViewController {
         navigationController?.pushViewController(signupController, animated: true)
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         navigationController?.isNavigationBarHidden = true
+        
+        view.addSubview(logoContainerView)
+        logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150)
         
         view.addSubview(signupButton)
         signupButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)

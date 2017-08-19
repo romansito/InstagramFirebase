f0460289196916c1389568ba201316ea8b358198
Roman Salazar Lopez <roman@Romans-MacBook-Pro.local>
2017-08-19 15:35:01 -0700

    presenting the new photoControllerView when pressing on the plus button

---
+++ InstagramFirebase/PhotoSelectorController.swift
@@ -0,0 +1,19 @@
+
+//
+//  PhotoSelecterController.swift
+//  InstagramFirebase
+//
+//  Created by Roman Salazar Lopez on 8/19/17.
+//  Copyright © 2017 Roman Salazar Lopez. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .yellow
+    }
+    
+}
